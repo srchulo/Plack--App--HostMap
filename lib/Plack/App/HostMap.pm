c66840048468b15543b48d3642c6913158f0fbcb
Adam Hopkins <SirChickenHair@srchulo-aire.local>
2014-08-04 12:02:51 -0500

    Can now handle multilevel subdomains, as suggested by singingfish. Also, added caching feature for subdomains that match rules. Also, foo.com and *.foo.com can have different apps now.

--- lib/Plack/App/HostMap.pm
+++ lib/Plack/App/HostMap.pm
@@ -24,9 +24,8 @@ sub _map_domain {
 
     Carp::croak("domain cannot be empty") unless $domain;
 
-    if($domain =~ /^\*\.(.*)$/) { 
-        $self->{_all_subdomains}->{$1} = 1;
-        $domain = $1;
+    if($domain =~ qr/^\*\./) { 
+        $self->{_dps} = 1;
     } 
 
     $self->{_map}->{$domain} = $app;
@@ -34,7 +33,7 @@ sub _map_domain {
 
 sub prepare_app { 
     my ($self) = @_;
-    $self->{_dps} = Domain::PublicSuffix->new if keys %{$self->{_all_subdomains}};
+    $self->{_dps} = Domain::PublicSuffix->new if $self->{_dps};
 }
 
 sub call {
@@ -46,10 +45,43 @@ sub call {
         $http_host =~ s/:$port$//;
     }
 
-    if($self->{_dps}) {
-        my $root = $self->{_dps}->get_root_domain($http_host);  
-        if($self->{_all_subdomains}->{$root} and $self->{_map}->{$root}) {
-            $http_host = $root;
+    #only enter this if there is not an exact match
+    if($self->{_dps} and not $self->{_map}->{$http_host}) {
+        if($self->{_cached_matches}->{$http_host}) {
+            $http_host = $self->{_cached_matches}->{$http_host};
+        }
+        else {
+            if($self->{_dps}->get_root_domain($http_host)) {
+                my $suffix = $self->{_dps}->suffix;
+
+                my ($subdomains) = $http_host =~ qr/^(.*)\.$suffix$/;
+                my @domains = split qr/\./, $subdomains;
+
+                my $match;
+                while(@domains) { 
+                    my $current_domain = join '.', @domains;
+                    $current_domain = "*.$current_domain.$suffix";
+
+                    if($self->{_map}->{$current_domain}) {
+                        $match = $current_domain;
+                        last;
+                    }
+
+                    shift @domains;
+                }
+
+                $match = $match ? $match : $http_host;
+                
+                #only cache if no_cache is false and we actually matched something
+                if(not $self->{no_cache} and $self->{_map}->{$match}) { 
+                    $self->{_cached_matches}->{$http_host} = $match;
+                }
+
+                $http_host = $match;
+            }
+            else { 
+                warn $self->{_dps}->error;
+            }
         }
     }
 
@@ -57,6 +89,16 @@ sub call {
 
     my $app = $self->{_map}->{$http_host};
     return $app->($env);
+}
+
+sub no_cache { 
+    my $self = shift;
+
+    if(@_) { 
+        $self->{no_cache} = shift;
+    }
+
+    return $self->{no_cache};
 }
  
 1;
@@ -70,6 +112,7 @@ __END__
     my $foo_app = sub { ... };
     my $bar_app = sub { ... };
     my $baz_app = sub { ... };
+    my $foo_bar_app= sub { ... };
  
     my $host_map = Plack::App::HostMap->new;
 
@@ -83,6 +126,9 @@ __END__
 
     #map all subdomains of a host to an app
     $host_map->map("*.foo.com" => $foo_app); #will match www.foo.com, foo.com, beta.foo.com, test.foo.com, beta.test.foo.com, etc...
+
+    #map multilevel subdomains of a host to an app
+    $host_map->map("*.foo.bar.com" => $foo_bar_app); #will match test.foo.bar.com, beta.foo.bar.com, beta.test.foo.bar.com, etc...
  
     my $app = $host_map->to_app;
  
@@ -114,7 +160,20 @@ If you need all subdomains of a host name to map to the same app, instead of lis
     $host_map->map("*.foo.com" => $foo_app); #will match www.foo.com, foo.com, beta.foo.com, test.foo.com, beta.test.foo.com, etc...
 
 This will map any subdomain of foo.com to C<$foo_app>. This way you can point new subdomains at your app without
-having to update your mappings.
+having to update your mappings. Also, L<Plack::App::Hostmap> will always match the most exact rule. For example, if you have the rules:
+
+    $host_map->map("foo.com" => $foo_app);
+    $host_map->map("*.foo.com" => $generic_foo_app);
+
+And you request C<foo.com>, it will match the C<$foo_app>, not the C<$generic_foo_app> since there is an explicit rule for C<foo.com>. 
+Also, if L<Plack::App::Hostmap> cannot find an exact match for a host, L<Plack::App::Hostmap> will always
+match the first rule it finds. For instance, if you have these two rules:
+
+    $host_map->map("*.beta.foo.com" => $beta_foo_app);
+    $host_map->map("*.foo.com" => $foo_app);
+
+And you request C<beta.foo.com>, it will match the C<$beta_foo_app>, not the C<$foo_app> because L<Plack::App::Hostmap> will find
+C<beta.foo.com> before C<foo.com> when looking for a match. 
  
 =method mount
  
@@ -129,8 +188,49 @@ Plack::App::HostMap object is callable (by overloading the code
 dereference), so returning the object itself as a PSGI application
 should also work.
  
+=method no_cache
+
+    $host_map->no_cache(1);
+
+    #or
+    my $host_map = Plack::App::HostMap->new(no_cache => 1);
+
+This method only applies if you are using the C<*.> syntax. By default, L<Plack::App::HostMap> will cache the corresponding
+mappings for a domain. For instance, if you have:
+
+    #beta.foo.com maps to *.foo.com
+    beta.foo.com -> *.foo.com
+
+Then after the first time that a url with the host C<beta.foo.com> is requested, the domain beta.foo.com will be stored in a hash as 
+a key with its value being C<foo.com>, to specify that that's what it maps to.
+If you are using the C<*.> syntax, it is strongly recommended that you do not turn this off because it could speed things up a lot since you avoid
+L<Domain::PublicSuffix>'s parsing logic, as well as some regex and logic that L<Plack::App::Hostmap> does to map the host to the right rule. However,
+one particular reason why you might want to disable caching would be if you were pointing A LOT of domains at your app. For instance, if you have the rule:
+
+    $host_map->map("*.foo.com" => $foo_app);
+
+And you request many urls with different C<foo.com> subdomains. This would take up a lot
+of memory since each host you requested to your app would be stored as a key in a hash. Keep in mind this would need to be very many, since even 1,000
+domains wouldn't take up much memory in a perl hash. Another possible reason
+to disable this would be that someone could potentially use it to crash your application/server. If you had this rule:
+
+    $host_map->map("*.foo.com" => $foo_app);
+
+And someone were to request many foo.com domains:
+
+    test.foo.com
+    test1.foo.com
+    test2.foo.com
+    ...
+
+Then each one would be cached as a key with its value being foo.com. If you are really worried about someone crashing your app, you could set L</no_cache> to 1, or instead of using
+the C<*.> syntax you could list out each individual host. Note: This only applies if you are using the C<*.> syntax. Otherwise, the hash
+that is used for caching is never even used. Also, in order to avoid letting the memory of your app grow uncontrollably, L<Plack::App::Hostmap> only caches hosts that
+actually map to a rule that you set. This way even if caching is on, someone can not make tons of requests with different hosts to your server and crash it.
+
 =head1 PERFORMANCE
  
+Note: This only applies if L</no_cache> is set to 1. 
 As mentioned in the L<DESCRIPTION|/"DESCRIPTION">, Plack::App::HostMap should perform
 much more efficiently than L<Plack::App::URLMap> when being used for host names. One
 caveat would be with the C<*.> syntax that can be used with L<map|/"map">. If you have even
@@ -138,9 +238,11 @@ just one mapping with a C<*.> in it:
 
     $host_map->map("*.foo.com" => $foo_app);
 
-Then on every request Plack::App::HostMap must call L<Domain::PublicSuffix>'s C<get_root_domain>
+Then on every request where the host is not an exact match for a rule (meaning that the host either matches a C<*.> syntax rule or no rule), 
+Plack::App::HostMap must call L<Domain::PublicSuffix>'s C<get_root_domain>
 subroutine to parse out the root domain of the host. I can't imagine that this is very costly, but
-maybe if you are receiving a lot of requests this could make a difference. If you find that to be the case,
+maybe if you are receiving a lot of requests this could make a difference. Also, L<Plack::App::Hostmap>
+does some additional logic to map your hosts. If you find that it is the case that it is affecting your performance,
 instead of using the C<*.> syntax you could list out each individual possibility:
 
     $host_map->map("beta.foo.com" => $foo_app);
